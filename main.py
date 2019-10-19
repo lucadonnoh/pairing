d733e848bd2bae4228ff2d51e7a993af486e20b1
Luca Donno <30298476+lucadonnoh@users.noreply.github.com>
2019-10-19 12:18:28 +0200

    updated square pairing adding more values

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def squarepairing():
     list_y = []
     list_x.append(x);
     list_y.append(y);
-    for i in range(100):
+    for i in range(150):
         if x == 0:
             x = y + 1
             y = 0
